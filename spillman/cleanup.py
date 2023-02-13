297719ded64231f211b26779023296f8f29cd24b
Lance <lhaynie@sccity.org>
2023-02-13 09:33:34 -0700

    fixing codacy issues

--- spillman/cleanup.py
+++ spillman/cleanup.py
@@ -16,7 +16,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-import json, logging, traceback
+import logging, traceback
 from .database import connect, connect_read
 from .log import setup_logger
 
